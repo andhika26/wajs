120cd74886c0b794ad82e4200bfd0b5dc8962521
Andhika Nur Afian <andhika.na@gmail.com>
2021-08-15 00:51:41 +0700

    PLKDEV

--- helpers/db.js
+++ helpers/db.js
@@ -2,9 +2,9 @@ const { query } = require('express-validator');
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgres://qmpccsewgydsmt:6f1bdce3546f3967d75c22b9968835b25c5fac26679373ff580adc0ac1a2183e@ec2-54-74-156-137.eu-west-1.compute.amazonaws.com:5432/dc0bp349ik6u2b',
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: true
   }
 });
 
